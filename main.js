de3ace3eae348a4620b229e71ffd846ee8443671
NaviDVC <ivan999az.vf@gmail.com>
2023-07-05 03:06:50 +0300

    ...

--- main.js
+++ main.js
@@ -1,35 +1,79 @@
-let choice = document.querySelectorAll(".item");
-let carsList = document.querySelector(".middle__cars-list");
-let phonesList = document.querySelector(".middle__phones-list");
-let companiesList = document.querySelector(".middle__companies-list");
-let buyButton = document.querySelectorAll(".middle__choose-item");
+let choice = document.querySelectorAll('.item');
+let categoriesBlock = document.querySelector('.left__block');
+let itemsBlock = document.querySelector('.middle__block');
+let buyBlock = document.querySelector('.right__block');
+let carsList = document.querySelector('.middle__cars-list');
+let phonesList = document.querySelector('.middle__phones-list');
+let buyButton = document.querySelectorAll('.middle__choose-item');
+let bmw = document.querySelector('.right__bmw');
+let ford = document.querySelector('.right__ford');
+let iphone = document.querySelector('.right__iphone');
+let nokia = document.querySelector('.right__nokia');
+let button = document.querySelectorAll('button')
 
 function cars() {
-  carsList.style.display = "block";
-  phonesList.style.display = "none";
-  companiesList.style.display = "none";
-  
+  carsList.style.display = 'block';
+  phonesList.style.display = 'none';
   console.log(buyButton);
 }
 
 function phones() {
-  carsList.style.display = "none";
-  phonesList.style.display = "block";
-  companiesList.style.display = "none";
-  
+  carsList.style.display = 'none';
+  phonesList.style.display = 'block';
   console.log(buyButton);
 }
 
-function companies() {
-  carsList.style.display = "none";
-  phonesList.style.display = "none";
-  companiesList.style.display = "block";
-  
-  console.log(buyButton);
+function bmwCar() {
+    bmw.style.display = 'block';
+    ford.style.display = 'none';
+    iphone.style.display = 'none';
+    nokia.style.display = 'none';
+    window.name = 'BMW M8 Competition';
 }
 
-choice[0].addEventListener("click", cars);
+function fordCar() {
+    bmw.style.display = 'none';
+    ford.style.display = 'block';
+    iphone.style.display = 'none';
+    nokia.style.display = 'none';
+    window.name = 'Ford Mustang Shelby GT500';
+}
 
-choice[1].addEventListener("click", phones);
+function iphoPhone() {
+    bmw.style.display = 'none';
+    ford.style.display = 'none';
+    iphone.style.display = 'block';
+    nokia.style.display = 'none';
+    window.name = 'iPhone 11 Pro Max 256GB';
+}
 
-choice[2].addEventListener("click", companies);
+function nokiaPhone() {
+    bmw.style.display = 'none';
+    ford.style.display = 'none';
+    iphone.style.display = 'none';
+    nokia.style.display = 'block';
+    window.name = 'Nokia 3310 0MB';
+}
+
+function resetWebsite() {
+    return categoriesBlock;
+}
+
+choice[0].addEventListener('click', cars);
+
+choice[1].addEventListener('click', phones);
+
+buyButton[0].addEventListener('click', bmwCar);
+
+buyButton[1].addEventListener('click', fordCar);
+
+buyButton[2].addEventListener('click', iphoPhone);
+
+buyButton[3].addEventListener('click', nokiaPhone);
+
+document.addEventListener('click', (event) => {
+    if (event.target.dataset.buy !== undefined) {
+        window.message = alert(`You bought ${name}!`)
+        location.reload()
+    }
+})
